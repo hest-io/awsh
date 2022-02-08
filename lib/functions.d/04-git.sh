deadd7336fa1a230c28677e10db64705c76aa07d
Hugh Mooney <hugh.mooney@gmail.com>
2022-02-08 15:37:17 +0000

    Added placeholder for git helper functions

---
+++ lib/functions.d/04-git.sh
@@ -0,0 +1,9 @@
+
+
+# Attempts to link current branch with matching branch at origin
+# _git_upstream_branch_match
+function _git_ubm {
+    local p_br_name="$(git branch --show-current)"
+    git branch --set-upstream-to="origin/${p_br_name}" "${p_br_name}"
+}
+
