3301c7217a3c83ad2b41f1939869988f392568ff
Lukasz Czarnota <luk.inc@gmail.com>
2022-03-10 18:21:40 +0100

    feat: bash history

--- lib/docker/entrypoint.sh
+++ lib/docker/entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Create shared bash history file if it doesn't exist
+[[ ! -f /home/awsh/.awsh/.bash_history ]] && umask 066 && touch /home/awsh/.awsh/.bash_history
+
+# Link shared history to user bash
+
+ln -s /home/awsh/.awsh/.bash_history /home/awsh/.bash_history
+
 # UID/GID (may) map to unknown user/group, $HOME=/ (the default when no home directory is defined)
 eval $( fixuid -q )
 # UID/GID now match user/group, $HOME has been set to user's home directory
