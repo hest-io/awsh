1ced5cf4cd00a853fc6b41bff40c66f68601cb93
Hugh Mooney <hugh.mooney@hest.io>
2023-09-18 12:39:03 +0000

    Fixes/cli

--- lib/functions.d/01-log.sh
+++ lib/functions.d/01-log.sh
@@ -57,3 +57,35 @@ function _log_ts {
     cat | while IFS= read -r line; do printf '%s %s %s\n' "$(date +'%Y-%m-%dT%H:%M:%S%z')" "$line" "$(echo -e ${__no_color})"; done
 }
 
+
+
+# A logging function which can be used as follows to exit the run with a specific error message
+# _log_exit_with_error "$LINENO" "Example test message"
+function _log_exit_with_error {
+
+  # ANSI escape code for red text
+  local red='\033[0;31m'
+  # ANSI escape code to reset text color
+  local reset='\033[0m'
+
+  local p_line_number="$1"
+  local p_message="$2"
+  : "${p_line_number:=0}"
+  : "${DEFAULT_OUT:=/dev/stderr}"
+
+  timestamp="$(date +'%Y-%m-%dT%H:%M:%S%z')"
+  log_file_msg=$(printf "[%s]: %-s:%04d : %s" "$timestamp" "$CONST_SCRIPT_NAME" "$p_line_number" "${p_message}")
+  log_tty_msg=$(printf "[%s]: %03d : %s" "$timestamp" "$p_line_number" "${red}ERROR ${p_message}${reset}")
+
+  # Output to TTY if enabled
+  [[ $TTY_OUTPUT_ENABLED -eq $TRUE ]] && echo -e "${log_tty_msg}" >&2
+  # Output to the DEFAULT_OUT file if enabled
+  [[ $LOG_OUTPUT_ENABLED -eq $TRUE ]] && echo -e "${log_file_msg}" >> "$DEFAULT_OUT"
+  # Output to the syslog if the logger command is found
+  _dummy=$(command -v logger > /dev/null 2>&1)
+  has_syslogger=$?
+  [[ $SYSLOG_OUTPUT_ENABLED -eq $TRUE ]] && [[ "$has_syslogger" -eq $TRUE ]] && logger -t "$CONST_SCRIPT_NAME[$$]" -p user.info "${log_tty_msg}"
+
+  exit 1
+
+}
